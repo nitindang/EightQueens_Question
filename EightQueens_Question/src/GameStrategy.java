82e447f2e33100fd539842cb5af12f01c90656ab
nitindang <nitindang14@gmail.com>
2021-07-09 00:48:39 +0530

    Changes for the Working code

--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -3,19 +3,20 @@ public class GameStrategy {
 	private int numQueens = 0;
 
 	private int getColumn(int cellId) {
-		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		// WRITE YOUR LOGIC HERE...................................
+		//Because with the increase in cellId the column changes
+		return cellId%8;
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		//Because row only increases after 8 squares
+		return cellId/8;
+
 	}
 
 	public boolean isValidPosition(int cellId) {
-		boolean isValid = true;
+
 
 		if(numQueens == 8) throw new GameOverException();	// just return out of the method
 
@@ -25,11 +26,46 @@ public class GameStrategy {
 		
 		int row = getRow(cellId);
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
+		// Is it already placed
+		if(placedQueens[row][col]==true){
+			return false;
+		}
+		else{
+			//Checking if a queen is already present in that entire row and column
+			for(int i = 0;i < 8;i++) {
+				if (placedQueens[row][i] || placedQueens[i][col]) {
+					return false;
+				}
+			}
+			//Now checking in diagonals
+			for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+				if(placedQueens[i][j]) {
+					return false;
+				}
+			}
 
-		*/
-		return isValid;
+			for(int i = row - 1, j = col + 1; i >= 0 && j< 8; i--, j++) {
+				if(placedQueens[i][j]) {
+					return false;
+				}
+			}
+
+			for(int i = row + 1, j = col - 1; i< 8 && j >= 0; i++, j--) {
+				if(placedQueens[i][j]) {
+					return false;
+				}
+			}
+
+			for(int i = row + 1, j = col + 1; i<8 && j<8; i++, j++) {
+				if(placedQueens[i][j]) {
+					return false;
+				}
+			}
+		}
+		// Because it didn't return false until now. Hence a valid position.
+		placedQueens[row][col]=true;
+		numQueens++;
+		return true;
 	}
 }
 
